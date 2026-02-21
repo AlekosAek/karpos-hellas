9915dcb98f19ef30cc84513527bebbc69ccfb0f8
John Doe <alexandroslenis@yahoo.com>
2026-02-21 12:10:54 +0100

    Some small changes

---
+++ script.js
@@ -0,0 +1,18 @@
+   tailwind.config = {
+            darkMode: "class",
+            theme: {
+                extend: {
+                    colors: {
+                        "primary": "#546a2f",
+                        "accent-gold": "#C5A059",
+                        "background-light": "#F9F8F3",
+                        "background-dark": "#1a1d15",
+                        "neutral-olive": "#787f6c",
+                    },
+                    fontFamily: {
+                        "display": ["Plus Jakarta Sans", "sans-serif"]
+                    },
+                    borderRadius: {"DEFAULT": "0.25rem", "lg": "0.5rem", "xl": "0.75rem", "full": "9999px"},
+                },
+            },
+        }
